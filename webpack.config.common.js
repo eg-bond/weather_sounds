e8d4b7da4b4c5b1f1fa2cbc69fa6536b5cf12283
eg-bond <eg-bond@yandex.ru>
2024-04-04 18:41:27 +0300

    build: favicon added

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
     }),
     new MiniCssExtractPlugin(),
     new CopyPlugin({
