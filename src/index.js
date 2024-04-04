e1016e1858c95b17e44cb0749bb29e02f8517a78
eg-bond <eg-bond@yandex.ru>
2024-04-04 20:12:55 +0300

    refactor: title in pure js done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles/index.scss'
 
-console.log('hello world')
+const root = document.getElementById('root')
+// title
+const title = document.createElement('h1')
+title.innerHTML = 'Weather sounds'
+title.classList.add('title')
+root.appendChild(title)
