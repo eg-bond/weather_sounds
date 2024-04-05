43e81450c8594d6af2803dca316927ceef4aad81
eg-bond <eg-bond@yandex.ru>
2024-04-05 13:58:57 +0300

    build: bundle-analyzer added

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,9 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const commonConfig = require('./webpack.config.common')
 const { merge } = require('webpack-merge')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [new BundleAnalyzerPlugin()],
 })
