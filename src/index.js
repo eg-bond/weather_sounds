075cbe134d44ca4fbd14089f9d7d2c3aa413bb85
eg-bond <eg-bond@yandex.ru>
2024-04-05 15:11:36 +0300

    feat: basic layout is made

--- src/index.js
+++ src/index.js
@@ -1,34 +1,46 @@
 import './styles/index.scss'
 import rainy_img from '@assets/rainy-bg.jpg'
-import rain_mp3 from '@assets/sounds/rain.mp3'
+import summer_img from '@assets/summer-bg.jpg'
+import winter_img from '@assets/winter-bg.jpg'
 
 const root = document.getElementById('root')
+// container
+const container = document.createElement('div')
+container.classList.add('container')
+// container.style.backgroundColor = 'red'
+container.style.backgroundImage = `url(${summer_img})`
+root.appendChild(container)
+
 // title
 const title = document.createElement('h1')
 title.innerHTML = 'Weather sounds'
 title.classList.add('title')
-root.appendChild(title)
+container.appendChild(title)
 
-// images
+// images --------------------------------------------------
 const images = document.createElement('div')
 images.classList.add('images')
-root.appendChild(images)
+container.appendChild(images)
+
+const img1_container = document.createElement('div')
+img1_container.classList.add('img_container')
 const img1 = document.createElement('img')
 img1.src = rainy_img
-images.appendChild(img1)
-root.appendChild(images)
+img1_container.appendChild(img1)
+images.appendChild(img1_container)
 
-// buttons to play sounds
-const buttons = document.createElement('div')
-buttons.classList.add('buttons')
-root.appendChild(buttons)
-const button1 = document.createElement('button')
-button1.innerHTML = 'Rainy'
-buttons.appendChild(button1)
-root.appendChild(buttons)
+const img2_container = document.createElement('div')
+img2_container.classList.add('img_container')
+const img2 = document.createElement('img')
+img2.src = summer_img
+img2_container.appendChild(img2)
+images.appendChild(img2_container)
 
-// event on click to button1 to play rainy sound
-button1.addEventListener('click', () => {
-  const audio = new Audio(rain_mp3)
-  audio.play()
-})
+const img3_container = document.createElement('div')
+img3_container.classList.add('img_container')
+const img3 = document.createElement('img')
+img3.src = winter_img
+img3_container.appendChild(img3)
+images.appendChild(img3_container)
+
+container.appendChild(images)
